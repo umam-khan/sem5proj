05e1d9e04f3006048d9590424e832ed8ed2b63ca
Umam Khan <umam.khan@outlook.com>
2023-10-25 02:07:44 +0530

    login logout added

--- SemV/src/main.jsx
+++ SemV/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-rijs4falok000vjn.us.auth0.com"
+    clientId="XhdDjbCeffab9qvGeibQtzN7e1QPfwLG"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  > <App />
+  </Auth0Provider>
+  ,
 )
--- SemV/src/pages/nav.jsx
+++ SemV/src/pages/nav.jsx
@@ -1,18 +1,54 @@
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0();  // Combining into a single line for cleaner code
+
+  // Optional: You might want to handle 'isLoading' to display a loading indicator
+
+  if (isLoading) {
+    return <div>Loading...</div>; // or some loading component
+  }
+
   return (
     <div>
       <nav>
         <div className="logo">UnHackMe</div>
         <ul>
-          <li><Link to="/scanner" style={{ textDecoration: 'none' }}>Scanner</Link></li>
-          <li><Link to="/password-manager" style={{ textDecoration: 'none' }}>Manager</Link></li>
-          <li><Link to="/password-maker" style={{ textDecoration: 'none' }}>Generator</Link></li>
+          <li>
+            <Link to="/scanner" style={{ textDecoration: "none" }}>
+              Scanner
+            </Link>
+          </li>
+          <li>
+            <Link to="/password-manager" style={{ textDecoration: "none" }}>
+              Manager
+            </Link>
+          </li>
+          <li>
+            <Link to="/password-maker" style={{ textDecoration: "none" }}>
+              Generator
+            </Link>
+          </li>
+          {!isAuthenticated ? (  // If not authenticated, show 'Log In'
+            <li>
+              <button onClick={() => loginWithRedirect()}>Log In</button>
+            </li>
+          ) : (
+            <li>
+              <button
+                onClick={() =>
+                  logout({ returnTo: window.location.origin })  // corrected 'logoutParams' to just 'returnTo'
+                }
+              >
+                Log Out
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
   );
-}
+};
 
 export default Navbar;
