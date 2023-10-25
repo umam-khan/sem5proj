247c43b73a59f2601b220c7f5e4ab66d6c8e65fb
umam-khan <umam.khan@outlook.com>
2023-10-25 21:52:32 +0530

    CSS changes

--- SemV/src/pages/nav.jsx
+++ SemV/src/pages/nav.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
-  const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0();  // Combining into a single line for cleaner code
+  const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0(); // Combining into a single line for cleaner code
 
   // Optional: You might want to handle 'isLoading' to display a loading indicator
 
@@ -13,32 +13,46 @@ const Navbar = () => {
   return (
     <div>
       <nav>
-        <div className="logo">UnHackMe</div>
+        <div className="logo" style={{ color: "purple" }}>
+          UnHackMe
+        </div>
         <ul>
           <li>
-            <Link to="/scanner" style={{ textDecoration: "none" }}>
+            <Link
+              to="/scanner"
+              style={{ color: "white", textDecoration: "none" }}
+            >
               Scanner
             </Link>
           </li>
           <li>
-            <Link to="/password-manager" style={{ textDecoration: "none" }}>
+            <Link
+              to="/password-manager"
+              style={{ color: "white", textDecoration: "none" }}
+            >
               Manager
             </Link>
           </li>
           <li>
-            <Link to="/password-maker" style={{ textDecoration: "none" }}>
+            <Link
+              to="/password-maker"
+              style={{ color: "white", textDecoration: "none" }}
+            >
               Generator
             </Link>
           </li>
-          {!isAuthenticated ? (  // If not authenticated, show 'Log In'
+          {!isAuthenticated ? ( // If not authenticated, show 'Log In'
             <li>
-              <button onClick={() => loginWithRedirect()}>Log In</button>
+              <button style={butsty} onClick={() => loginWithRedirect()}>
+                Log In
+              </button>
             </li>
           ) : (
             <li>
               <button
-                onClick={() =>
-                  logout({ returnTo: window.location.origin })  // corrected 'logoutParams' to just 'returnTo'
+                style={butsty}
+                onClick={
+                  () => logout({ returnTo: window.location.origin }) // corrected 'logoutParams' to just 'returnTo'
                 }
               >
                 Log Out
@@ -51,4 +65,11 @@ const Navbar = () => {
   );
 };
 
+const butsty = {
+  width: "130px",
+  height: "40px",
+  borderRadius: "10px",
+  backgroundColor: "white",
+  color: "black",
+};
 export default Navbar;
